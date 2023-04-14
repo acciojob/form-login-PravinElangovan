ce44574ec7082b2b62c2809c2b557cf3dc95e322
PravinElangovan <115397005+PravinElangovan@users.noreply.github.com>
2023-04-14 12:42:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	var firstName = document.getElementById("fname").value;
+	var secondName = document.getElementById("lname").value;
+	alert(`${firstName} ${secondName}`);
 }
